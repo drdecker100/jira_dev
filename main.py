83fc9c81e3d5e5481db218c87b3dcf74d80587f2
drdecker100 <57122130+drdecker100@users.noreply.github.com>
2022-07-01 01:49:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
+# https://developer.atlassian.com/cloud/jira/platform/rest/v3/
 import requests
 import json
 from masterfile import auth
 
 issueIdOrKey = "SSP-5"
 
-url = f"https://drdecker100.atlassian.net/rest/api/3/issue/{issueIdOrKey}"   
+def check_response_valid():         # check if response is valid
+    url = f"https://drdecker100.atlassian.net/rest/api/3/issue/{issueIdOrKey}"   
+    headers = {"Accept": "application/json"}
+     
+    response = requests.request("GET",url,headers=headers,auth=auth)
+    return response
 
-headers = {"Accept": "application/json"}
+def get_issue():                     # fetch issue with key or id  
+    resp = json.dumps(json.loads(res.text), sort_keys=True, indent=4, separators=(",", ": "))
+    response = json.loads(resp)      # convert json obj to dict
+    return response
 
-response = requests.request("GET",url,headers=headers,auth=auth)
-
-def get_issue(resp): # fetch issue with key or id  https://developer.atlassian.com/cloud/jira/platform/rest/v3/
-    res = json.dumps(json.loads(resp.text), sort_keys=True, indent=4, separators=(",", ": "))
-    res_d = json.loads(res)      #convert json to dict
-    return res_d
-
-def check_if_attachment_exist(): # check if issue has attachment
-    res_d = get_issue(response)
+def check_if_attachment_exist():     # check if issue has attachment
     if res_d["fields"]["attachment"]:
         return True
     return False
 
-def get_attachments():           # fetch all atachments
+def get_attachments():               # fetch all atachments
     if check_if_attachment_exist():
-        res_d = get_issue(response)
         metadata = res_d["fields"]["attachment"]
 
-        for i, v in enumerate(metadata):
-            attachment = res_d["fields"]["attachment"][i]
+        for _, attachment in enumerate(metadata):          
             content_url = attachment["content"]
             filename = attachment["filename"]
             res_b = requests.get(content_url, auth=auth)
@@ -37,8 +36,16 @@ def get_attachments():           # fetch all atachments
         
     else:
         print("No attachment to this issue")
-   
 
-# call functions
-get_issue(response)
-get_attachments()
+def get_issue_metadata():
+    pass
+
+             
+# Call function
+res = check_response_valid()
+if res.status_code == 200:
+    print(f"Response status code: {res.status_code}")
+    res_d = get_issue()
+    get_attachments()
+else:
+    print(f"Response status code: {res.status_code}")
